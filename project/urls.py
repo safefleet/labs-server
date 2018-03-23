54d4c3edb1f66227de8043b89a72dc26db5e3fd3
Luci Furtun <lucianfurtun@gmail.com>
2018-03-23 11:51:03 +0200

    Add some initial settings

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""labsserver URL Configuration
+"""Labs Server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
