c32f543284d64a02b16559dc36f36ebd6a0d57d5
Tudor <gltudor96@gmail.com>
2018-04-24 14:23:05 +0300

    added serializers

--- VehicleManagement/models.py
+++ VehicleManagement/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
 
+class Vehicle(models.Model):
+    type = models.CharField(max_length = 30)
+    color = models.CharField(max_length = 30)
+
+
+class Journey(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    car = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+
+
 class Position(models.Model):
-    car = models.ForeignKey(Journey, on_delete=models.CASCADE())
+    car = models.ForeignKey(Journey , on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
 
 
-class Vehicle(Model):
-    type = CharField(max_length = 30)
-    color = CharField(max_length = 30)
 
 
-class Journey(Model):
-    start_time = DateTimeField()
-    end_time = DateTimeField()
-    car = ForeignKey(Vehicle, on_delete=Cascade)
---
+++ VehicleManagement/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from VehicleManagement.models import Position,Vehicle,Journey
+
+class PositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Position
+        fields = ('car', 'longitude', 'latitude')
+
+class JourneySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Position
+        fields = ('start_time', 'end_time', 'car')
+
+
+
