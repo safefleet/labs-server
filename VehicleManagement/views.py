2715afe296b2750d32db9c802cfadfa2a62d5309
Adina Țeudan <adina.teudan@gmail.com>
2018-04-24 17:40:48 +0300

    GET/PUT/PATCH/DELETE ->/api/vehicles/<id>/

--- VehicleManagement/views.py
+++ VehicleManagement/views.py
@@ -14,3 +14,36 @@ def vehicle_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def vehicle_detail(request, pk):
+    """
+    Retrieve, update or delete a vehicle.
+    """
+    try:
+        vehicle = Vehicle.objects.get(pk=pk)
+    except Vehicle.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = VehicleSerializer(vehicle)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = VehicleSerializer(vehicle, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PATCH':
+        serializer = VehicleSerializer(vehicle, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        vehicle.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
