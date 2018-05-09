d64013fd1f940b9dd1b720e36ecefe2ce493d3d3
Luci Furtun <lucianfurtun@gmail.com>
2018-05-09 13:52:24 +0300

    Remove unused imports

--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,11 @@
 from rest_framework import status, generics
-from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
-from rest_framework_jwt.views import ObtainJSONWebToken, RefreshJSONWebToken, VerifyJSONWebToken
+from rest_framework.views import APIView
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-
-from django.contrib.auth import login, authenticate
+from rest_framework_jwt.views import ObtainJSONWebToken, RefreshJSONWebToken, VerifyJSONWebToken
 
 from authentication.models import User
-
 from .serializers import UserSerializer
 
 
@@ -23,27 +20,29 @@ class AuthRegister(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class AuthLogin(ObtainJSONWebToken):
     pass
-    
+
+
 class AuthVerify(VerifyJSONWebToken):
     pass
+
 
 class AuthRefresh(RefreshJSONWebToken):
     pass
 
+
 class UserList(generics.ListCreateAPIView):
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
     queryset = User.objects.all()
 
-    
 
 class UserDetail(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (JSONWebTokenAuthentication, )
-    permission_classes = (AllowAny, )
-
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (AllowAny,)
 
     def get_object(self):
         user = self.request.user
