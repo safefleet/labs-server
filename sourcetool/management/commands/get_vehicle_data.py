c77e22428df11c88978ad35d9cef3b7d301ea5b2
PaulakaPaul <p.e.iusztin@gmail.com>
2018-05-14 18:18:18 +0300

    Data thrown in the channels and protected settings

--- sourcetool/management/commands/get_vehicle_data.py
+++ sourcetool/management/commands/get_vehicle_data.py
@@ -7,7 +7,6 @@ import async_timeout
 import time
 
 import json
-import redis
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -15,17 +14,12 @@ from django.conf import settings
 
 class Command(BaseCommand):
 
-    def add_arguments(self, parser):
-        # parser.add_argument('--show', action='store_true', dest='show')
-        pass
-
     def handle(self, *args, **options):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self._main())
 
     async def _main(self):
 
-        redis_que = RedisQueue('vehicles', charset='utf-8', decode_responses=True)
         channel_layer = channels.layers.get_channel_layer()
 
         async with aiohttp.ClientSession() as session:
@@ -37,22 +31,27 @@ class Command(BaseCommand):
                     'https://alpha.safefleet.eu/safefleet/api/get_current_positions',
                     self._create_login_params())
 
-                # mapped json
-                new_vehicle_data = self.adapt_all_vehicle_data(req_all_data)
+                # mapped dict
+                new_vehicle_data, new_position_data = self.adapt_all_data(req_all_data)
+                # tuple with (vehicle, position) class
 
+                # TODO add desired urls
                 # post
-                await self.post_all_vehicle_data(
+                # await self.post_all_vehicle_data(
+                #     session,
+                #     'url',
+                #     new_vehicle_data)  # post vehicles
+
+                await self.post_all_position_data(
                     session,
                     'http://127.0.0.1:8000/sourcetool/',
-                    new_vehicle_data)
+                    new_position_data)  # post positions
 
-                # save to redis que
+                # save to redis que as json
                 json_vehicle_data = json.dumps(new_vehicle_data)
-                await channel_layer.send(settings.CHANNEL_NAME, {settings.JSON_DATA_KEY: json_vehicle_data})
-
-                # testing code
-                # unpacked = json.loads(redis_que.get())
-                # print(unpacked[0]['vehicle_id'])
+                json_location_data = json.dumps(new_position_data)
+                await channel_layer.send(settings.CHANNEL_NAME, {settings.JSON_DATA_VEHICLES_KEY: json_vehicle_data,
+                                                                 settings.JSON_DATA_LOCATIONS_KEY: json_location_data})
 
                 asyncio.sleep(1)
 
@@ -63,23 +62,35 @@ class Command(BaseCommand):
 
     async def post_all_vehicle_data(self, session, url, all_adapted_vehicle_data):
         async with session.post(url, json=all_adapted_vehicle_data) as resp:
-            print('Response:\n {}'.format(await resp.text()))
+            print('Response from vehicles post:\n {}'.format(await resp.text()))
 
-    def adapt_all_vehicle_data(self, all_vehicle_data) -> list:
+    async def post_all_position_data(self, session, url, all_adapted_position_data):
+        async with session.post(url, json=all_adapted_position_data) as resp:
+            print('Response from position post:\n {}'.format(await resp.text()))
+
+    def adapt_all_data(self, all_vehicle_data) -> tuple:
         new_vehicle_data = []
+        new_position_data = []
+
         for vehicle_data in all_vehicle_data:
-            new_vehicle_data.append(self.adapt_vehicle_data(vehicle_data))
+            new_data = self.adapt_data(vehicle_data)
 
-        return new_vehicle_data
+            new_vehicle_data.append(new_data[0])
+            new_position_data.append(new_data[1])
 
-    def adapt_vehicle_data(self, vehicle_data) -> dict:
-        return {'vehicle_id': vehicle_data['vehicle']['vehicle_id'],
-                'vehicle': {'number': vehicle_data['vehicle']['license_plate'],
-                            'type': vehicle_data['vehicle']['maker'] + " " + vehicle_data['vehicle']['model']},
-                'position': {'latitude': vehicle_data['lat'],
-                             'longitude': vehicle_data['lng'],
-                             'moment': vehicle_data['moment']}
-                }
+        return new_vehicle_data, new_position_data
+
+    def adapt_data(self, vehicle_data) -> tuple:  # (vehicle, position)
+        return ({'vehicle_id': vehicle_data['vehicle']['vehicle_id'],
+                 'vehicle': {'number': vehicle_data['vehicle']['license_plate'],
+                             'type': vehicle_data['vehicle']['maker'] + " " + vehicle_data['vehicle']['model']}
+                 },
+                {'vehicle_id': vehicle_data['vehicle']['vehicle_id'],
+                 'position': {'latitude': vehicle_data['lat'],
+                              'longitude': vehicle_data['lng'],
+                              'moment': vehicle_data['moment']}
+                 }
+                )
 
     def _create_login_params(self) -> dict:
         timestamp = time.time()
@@ -89,44 +100,3 @@ class Command(BaseCommand):
 
         return {'client_id': settings.CLIENT_ID, 'timestamp': int(timestamp),
                 'signature': signature}
-
-
-class RedisQueue(object):
-    """Simple Queue with Redis Backend"""
-    def __init__(self, name, namespace='queue', **redis_kwargs):
-        """The default connection parameters are: host='localhost', port=6379, db=0"""
-        self.__db = redis.Redis(**redis_kwargs)
-        self.key = '%s:%s'.format(namespace, name)
-
-    def qsize(self):
-        """Return the approximate size of the queue."""
-        return self.__db.llen(self.key)
-
-    def empty(self):
-        """Return True if the queue is empty, False otherwise."""
-        return self.qsize() == 0
-
-    def put(self, item):
-        """Put item into the queue."""
-        self.__db.rpush(self.key, item)
-
-    def get(self, block=True, timeout=None):
-        """Remove and return an item from the queue.
-
-        If optional args block is true and timeout is None (the default), block
-        if necessary until an item is available."""
-        if block:
-            item = self.__db.blpop(self.key, timeout=timeout)
-        else:
-            item = self.__db.lpop(self.key)
-
-        # if item:
-        if block:
-            item = item[1]
-
-        return item
-
-    def get_nowait(self):
-        """Equivalent to get(False)."""
-        return self.get(False)
-
