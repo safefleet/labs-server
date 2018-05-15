28cfb4dd5f8136cbff197635206227965a2eaac7
PaulakaPaul <p.e.iusztin@gmail.com>
2018-05-15 21:14:07 +0300

    Deleted extra configs in settings file

--- project/settings.py
+++ project/settings.py
@@ -121,18 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Channels
-ASGI_APPLICATION = 'mysite.routing.application'
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
-
-
 try:
     from .local_settings import *
 except ImportError:
