50acfd7d26ec4312a11a27d2233c0a6c89776dfe
Adina Țeudan <adina.teudan@gmail.com>
2018-05-23 15:55:27 +0300

    Added docs and prefix '/api' to vehicles

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 from project import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vehicles/', include('vehicle.urls')),
+    path('api/vehicles/', include('vehicle.urls')),
     path('api/auth/', include('authentication.urls')),
+    path('docs/', include_docs_urls(title='Safefleet LABS',
+                                    authentication_classes=[],
+                                    permission_classes=[]))
 ]
