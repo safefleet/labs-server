da18318e7bded01b7ba77d743a2d370d9a98b63a
Luci Furtun <lucianfurtun@gmail.com>
2018-05-24 12:23:02 +0300

    Rename websocket routing

--- liveServer/routing.py
+++ liveServer/routing.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from django.urls import path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/position/(?P<vehicle_id>\d+)/$', consumers.VehicleConsumer),
-    #path('ws/position/<int:vehicle_id>', consumers.VehicleConsumer),
+    url(r'^position/(?P<vehicle_id>\d+)/$', consumers.VehicleConsumer),
 ]
