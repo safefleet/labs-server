e7dd6656279dd2e0f4f3c02dee35ad0639036ce1
PaulakaPaul <p.e.iusztin@gmail.com>
2018-06-14 12:11:58 +0300

    Deleted unnecessary files

--- sourcetool/views.py
+++ sourcetool/views.py
@@ -5,9 +5,3 @@ from django.urls import reverse
 
 # Create your views here.
 
-
-# @csrf_protect
-def post_listener(request):
-    context = {'data': request.POST}
-    print(context)
-    return render(request, 'sourcetool/seepost.html', context)
